71eff9eeda6674cf6ff76186b3b7ddafa951999a
SubJunk <subjunk@gmail.com>
2015-03-20 12:03:35 +1300

    Sync DLNAResource.getDlnaOrgPnFlags() with b474e16

--- src/main/java/net/pms/dlna/DLNAResource.java
+++ src/main/java/net/pms/dlna/DLNAResource.java
@@ -2030,7 +2030,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 										}
 
 										if (!finishedMatchingPreferences && params.sid == null) {
-											st = new StringTokenizer(configuration.getSubtitlesLanguages(), ",");
+											st = new StringTokenizer(UMSUtils.getLangList(params.mediaRenderer), ",");
 											while (st.hasMoreTokens()) {
 												String lang = st.nextToken();
 												lang = lang.trim();
