7e30cea8595e96c625e9663b3ae54fb1c7d65a93
Nadahar <magne.sandve@gmail.com>
2015-07-06 16:30:54 +0200

    Rewrote PMS.isHeadless() so that the actual check is done only once
    Conflicts:
    	src/main/java/net/pms/PMS.java

--- src/main/java/net/pms/PMS.java
+++ src/main/java/net/pms/PMS.java
@@ -1005,7 +1005,6 @@ public class PMS {
 
 	public static void main(String args[]) throws IOException, ConfigurationException {
 		boolean displayProfileChooser = false;
-		boolean headless = true;
 
 		if (args.length > 0) {
 			for (String arg : args) {
@@ -1041,9 +1040,7 @@ public class PMS {
 				if (System.getProperty(NOCONSOLE) == null) {
 					System.setProperty(CONSOLE, Boolean.toString(true));
 				}
-			} else {
-				headless = false;
-			}
+			} 
 		} catch (Throwable t) {
 			LOGGER.error("Toolkit error: " + t.getClass().getName() + ": " + t.getMessage());
 
@@ -1052,7 +1049,7 @@ public class PMS {
 			}
 		}
 
-		if (!headless && displayProfileChooser) {
+		if (!isHeadless() && displayProfileChooser) {
 			ProfileChooser.display();
 		}
 
@@ -1106,7 +1103,7 @@ public class PMS {
 
 			LOGGER.error(errorMessage);
 
-			if (!headless && instance != null) {
+			if (!isHeadless() && instance != null) {
 				JOptionPane.showMessageDialog(
 					(SwingUtilities.getWindowAncestor((Component) instance.getFrame())),
 					errorMessage,
@@ -1357,18 +1354,23 @@ public class PMS {
 		PlayerFactory.registerPlayer(player);
 	}
 
-	/*
+	private static Boolean headless = null;
+	
+	/**
 	 * Check if UMS is running in headless (console) mode, since some Linux
 	 * distros seem to not use java.awt.GraphicsEnvironment.isHeadless() properly
 	 */
 	public static boolean isHeadless() {
-		try {
-			javax.swing.JDialog d = new javax.swing.JDialog();
-			d.dispose();
-			return false;
-		} catch (java.lang.NoClassDefFoundError | java.awt.HeadlessException | java.lang.InternalError e) {
-			return true;
-		}
+		if (headless == null) {
+			try {
+				JDialog d = new JDialog();
+				d.dispose();
+				headless = new Boolean(false);
+			} catch (NoClassDefFoundError | HeadlessException | InternalError e) {
+				headless = new Boolean(true);				
+			}
+		}		
+		return headless.booleanValue();
 	}
 
 	private RemoteWeb web;
